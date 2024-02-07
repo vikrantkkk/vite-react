88eb4f04e9c9909b4cac14a5a0c05f0fc4a86612
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-02-07 22:03:09 +0530

    Revert "component"
    This reverts commit 85e239779ab30b229650daf479c208b8288fdbc8.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 
 import './App.css'
-import Mycomponents from './Mycomponents'
-import MyComponent from './MyComponent'
-import Greeting from './greeting/Greeting'
+
 function App() {
 
   return (
     <>
      <div>React Vite</div>
-     <Mycomponents/>
-     <MyComponent/>
-     <Greeting/>
     </>
   )
 }
