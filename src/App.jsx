460ca13d13ff5a087fec236413a971953a144e62
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-03-14 22:31:06 +0530

    twitter structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import Example from "./Example";
 import List from "./List";
 import Parent from "./Parent";
 import ParentComponent from "./ParentComponent";
+import Home from "./components/home/Home";
 import Vikrant from "./ragini";
 
 function App() {
   const color = "red";
   return (
     <>
-      <ParentComponent color={color} />
+      {/* <ParentComponent color={color} />
       <Example/>
       <Parent/>
       <Vikrant />
-      <List/>
-
+      <List/> */}
+      <Home />
     </>
   );
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ display: "flex" }}>
+      <div
+        style={{ width: "30vw", border: "1px solid black", height: "100vh" }}
+      >
+        1111111111111111111
+      </div>
+      <div
+        style={{ width: "40vw", border: "1px solid black", height: "100vh" }}
+      >
+        <div
+          style={{ width: "100%", height: "23vh", border: "2px solid red" }}
+        ></div>
+         <div
+          style={{
+            width: "100%",
+            height: "77vh",
+            border: "2px solid yellow",
+            overflowY:"scroll"
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque corrupti optio ipsam id expedita nesciunt vero. Asperiores repellendus totam quos dicta nam aut aperiam, recusandae, iure rem repellat repudiandae nisi.
+        </div>
+      </div>
+      <div
+        style={{
+          width: "30vw",
+          border: "1px solid black",
+          height: "100vh",
+        }}
+      >
+        3
+      </div>
+    </div>
+  );
+};
+
+export default Home;
